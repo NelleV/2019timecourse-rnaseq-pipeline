d901d93c37edf9f4840a445f01c03c331fa0c511
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-01-14 10:23:57 -0800

    ENH adding processing step, to format all dataset identically

---
+++ scripts/step0_preprocessing/process_epicon_leaf.R
@@ -0,0 +1,42 @@
+
+###############################################################################
+# Options
+#
+
+data_dir = "data/varoquaux2019/leaf/"
+results_dir = "results/varoquaux2019/leaf/"
+
+###############################################################################
+# Load the data
+counts = read.delim(file.path(data_dir, "data.tsv"),
+    		    sep="\t",
+		    stringsAsFactors=FALSE,
+		    check.names=FALSE)
+meta = read.table(file.path(data_dir, "meta.tsv"),
+		  check.names=FALSE)
+
+###############################################################################
+# The pipeline requires:
+#   - a Group column in the metadata file, containing the "group" used to do
+#     the time-course differential expression analysis
+#   - a Time colmun, containing the time variable (in the case of EPICON, the
+#     week
+meta$Time = as.numeric(as.character(meta$Week))
+ng_labels = as.factor(
+  make.names(meta$Condition:meta$Genotype))
+meta$Group = ng_labels
+
+
+###############################################################################
+# Now, just save in the folder
+outname = file.path(results_dir, "data.tsv")
+write.table(counts, file=outname, sep="\t", row.names=TRUE, col.names=TRUE)
+
+outname = file.path(results_dir, "meta.tsv")
+write.table(meta, file=outname, sep="\t", row.names=TRUE, col.names=TRUE)
+
+file.copy(file.path(data_dir, "contrasts"),
+	  file.path(results_dir, "contrasts"))
+
+file.copy(file.path(data_dir, "config.yml"),
+	  file.path(results_dir, "config.yml"))
