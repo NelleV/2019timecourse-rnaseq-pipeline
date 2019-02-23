01c91fc7477202d58e69fa7c3ce9a0d817b35c4a
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-02-22 16:22:26 -0800

    FIX More text + small fixes in QC + normalization

--- scripts/utils.R
+++ scripts/utils.R
@@ -5,10 +5,14 @@ group_colors = c(
     "VH"="red4",
     "VL"="red2")
 time_colors = grDevices::rainbow(14)
-names(time_colors) = c(0, 3, 6, 9, 12, 18, 24, 30, 48, 60, 72, 120, 168)
+names(time_colors) = c(0, 3, 6, 9, 12, 18, 24, 30, 36, 48, 60, 72, 120, 168)
 
 ann_colors = list(
     Time=time_colors,
     Group=group_colors
     )
 
+replicate_markers = c(15, 17, 19)
+names(replicate_markers) = c(1, 2, 3)
+ann_markers = list(
+    Replicate=replicate_markers)
