1c8a17e9ca1081f24dc88c8bb87fcc2b02110feb
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-03-05 13:45:14 -0800

    FIX download data

--- data/sync_shoemaker2015.sh
+++ data/sync_shoemaker2015.sh
@@ -1,1 +1,4 @@
-wget -r http://members.cbio.mines-paristech.fr/~nvaroquaux/data/rnaseq/shoemaker2015/GSE63786_RAW.tar
+wget -r \
+http://members.cbio.mines-paristech.fr/~nvaroquaux/data/rnaseq/shoemaker2015/GSE63786_RAW.tar \
+-o GSE63786_RAW.tar
+
