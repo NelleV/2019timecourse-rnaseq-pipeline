b9332d861ae39ba442bf01e3eee9d22d56c67a5c
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-03-05 09:12:10 -0800

    Adding travis configuration + scripts

---
+++ .build_tools/travis/test_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
+
+set -e
+
+run_tests() {
+    # first run the actual tests
+    pushd bin/moanin
+    make test
+    popd bin/moanin
+    pushd scripts
+    make all
+    popd
+}
+
+run_tests
