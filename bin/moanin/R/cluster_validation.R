5b6155d2f844bb8b92077e1841e1fb7d05479d67
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-04-24 09:14:05 -0700

    ENH Now performing prediction step in stability analysis

--- bin/moanin/R/cluster_validation.R
+++ bin/moanin/R/cluster_validation.R
@@ -1,9 +1,23 @@
 library(reshape2)
 
 
-consensus_matrix = function(labels){
-    melted_labels = reshape2::melt(labels)
+#' Compute consensus matrix from labels
+#'
+#' @param labels matrix (n_genes, n_clusters)
+#'	Matrix containing the set of labels
+#' @param scale	boolean, optional, default: TRUE
+#'	Whether to rescale the matrix
+#' @export
+consensus_matrix = function(labels, scale=TRUE){
+    melted_labels = reshape2::melt(as.matrix(labels))
     colnames(melted_labels) = c("Gene", "Clustering", "Label")
     melted_labels = subset(melted_labels, select=c("Gene", "Label"))
-    return(tcrossprod(table(melted_labels)))
+    melted_labels = melted_labels[!is.na(melted_labels["Label"]),]
+    consensus = tcrossprod(table(melted_labels)) 
+    if(scale){
+        diag_elements = diag(consensus)
+	consensus = t(t(consensus / diag_elements) / diag_elements)
+	diag(consensus) = 0
+    }
+    return(consensus)
 }
