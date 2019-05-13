6f4428f7bb0ae4e66944c7dbecd6658c4e793bdc
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-05-13 15:04:10 -0700

    Added TODO items

--- scripts/.scripts/compile_rmarkdown_pdf.R
+++ scripts/.scripts/compile_rmarkdown_pdf.R
@@ -5,7 +5,3 @@ filename = args[1]
 outname = args[2]
 
 render(filename, output_file=outname)
-draft("MyArticle.Rmd",
-      template="f1000_article",
-      package="BiocWorkflowTools",
-      edit=FALSE)
